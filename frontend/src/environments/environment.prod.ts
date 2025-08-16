b9b6ee906b9adecb64a206dd9495a0ed1f0db797
AdityaPS1209 <adityapratapsm1bill@gmail.com>
2025-08-17 00:41:39 +0530

    Feat: Configure production      API URL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8000' // This should be replaced with the actual production API URL
+  apiUrl: 'https://cards-book-backend.onrender.com'
 };
